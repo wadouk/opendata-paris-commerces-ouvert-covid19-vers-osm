19b2da1b8dca5a7e3cad7930a632930aa72ccf18
Nicolas Bétheuil <wadouk@gmail.com>
2020-05-13 20:00:39 +0200

    update

--- taf.sh
+++ taf.sh
@@ -1,2 +1,3 @@
 curl -L "https://opendata.paris.fr/explore/dataset/coronavirus-commercants-parisiens-livraison-a-domicile/download/?format=json&timezone=Europe/Berlin&lang=fr" > coronavirus-commercants-parisiens-livraison-a-domicile.json
 node in.js > out.geojson
+curl -v -F "geojson=@out.geojson" https://od2osm.cleverapps.io/api/quests -H "Content-Type: multipart/form-data"
